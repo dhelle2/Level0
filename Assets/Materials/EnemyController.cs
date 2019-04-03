de0d70f803be0196ce42ae7553bb2d838a6a0d4a
Jay <jpatel360@gmail.com>
2019-04-02 22:44:51 -0500

    check

---
+++ Assets/Materials/EnemyController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform player;
+    public Transform myTransform;
+    Animator anim;
+    Vector3 startPos;
+
+
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+      
+        startPos = this.transform.position;
+
+    }
+
+    IEnumerator Example()
+    {
+
+        yield return new WaitForSeconds(2);
+        this.transform.position = startPos;
+
+       // anim.ResetTrigger("trigger1");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float distance = Vector3.Distance(player.position, transform.position);
+
+
+        if (distance < 10)
+        {
+            anim.SetTrigger("trigger1");
+
+            StartCoroutine(Example());
+            anim.ResetTrigger("trigger1");
+        }
+
+
+
+        transform.LookAt(player);
+        transform.Translate(Vector3.forward * 5 * Time.deltaTime);
+
+    }
+}
