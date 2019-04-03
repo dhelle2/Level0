83ed86767571b5766ba67d79220e03340ca9bbbf
dhelle2 <dhelle2@uic.edu>
2019-04-03 11:39:12 -0500

    room1 audio stop

--- Assets/escape.cs
+++ Assets/escape.cs
@@ -10,11 +10,14 @@ public class escape : MonoBehaviour
     public GameObject player2;
     public InputField inputField;
     public GameObject door;
+    public GameObject doorButton;
+    public AudioSource room1Audio;
+    public GameObject room1;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //doorButton.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,11 +29,15 @@ public class escape : MonoBehaviour
     public void checkEscapeAndEscape() {
         if (inputField.text == "12")
         {
+            //doorButton.gameObject.SetActive(true);
+            room1Audio = room1.GetComponent<AudioSource>();
+            room1Audio.Stop();
             //player1.GetComponent<Rigidbody>().transform.position += new Vector3(0F, 0F, -100F);
             //player2.GetComponent<Rigidbody>().transform.position += new Vector3(0F, 0F, -100F);
             door.SetActive(false);
             player1.GetComponent<Rigidbody>().useGravity = false;
             player2.GetComponent<Rigidbody>().useGravity = false;
+
         }
     }
 }
