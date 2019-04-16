7a418a4de9f2ecd0e45cdf34a651651fb73eaf23
ydimou <ydimou@gmail.com>
2019-04-15 23:34:45 -0500

    GUI Triggers
    GUI now activates when approaching door and deactivates after passing through.
    
    Added Player 2 Drone with #2
    
    Made each drone only visible by one player.

---
+++ Assets/Keypad.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keypad : MonoBehaviour
+{
+
+    public string curPassword = "12345";
+    public string input;
+    public bool onTrigger;
+    public bool doorOpen;
+    public bool keypadScreen;
+    public GameObject Player1;
+    public GameObject Player2;
+    public GameObject keypad1;
+    //public Transform doorHinge;
+
+    void Start()
+    {
+        Player1.GetComponent<Transform>();
+        Player2.GetComponent<Transform>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        onTrigger = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        onTrigger = false;
+        keypadScreen = false;
+        input = "";
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(Player1.transform.position, keypad1.transform.position) < 2)
+        {
+            print("heyyy");
+        }
+
+        if (input == curPassword)
+        {
+            doorOpen = true;
+        }
+
+        if (doorOpen)
+        {
+            //var newRot = Quaternion.RotateTowards(doorHinge.rotation, Quaternion.Euler(0.0f, -90.0f, 0.0f), Time.deltaTime * 250);
+            //doorHinge.rotation = newRot;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (!doorOpen)
+        {
+            if (onTrigger)
+            {
+                GUI.Box(new Rect(0, 0, 200, 25), "Press 'E' to open keypad");
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    keypadScreen = true;
+                    onTrigger = false;
+                }
+            }
+
+            if (keypadScreen)
+            {
+                GUI.Box(new Rect(0, 0, 320, 455), "");
+                GUI.Box(new Rect(5, 5, 310, 25), input);
+
+                if (GUI.Button(new Rect(5, 35, 100, 100), "1"))
+                {
+                    input = input + "1";
+                }
+
+                if (GUI.Button(new Rect(110, 35, 100, 100), "2"))
+                {
+                    input = input + "2";
+                }
+
+                if (GUI.Button(new Rect(215, 35, 100, 100), "3"))
+                {
+                    input = input + "3";
+                }
+
+                if (GUI.Button(new Rect(5, 140, 100, 100), "4"))
+                {
+                    input = input + "4";
+                }
+
+                if (GUI.Button(new Rect(110, 140, 100, 100), "5"))
+                {
+                    input = input + "5";
+                }
+
+                if (GUI.Button(new Rect(215, 140, 100, 100), "6"))
+                {
+                    input = input + "6";
+                }
+
+                if (GUI.Button(new Rect(5, 245, 100, 100), "7"))
+                {
+                    input = input + "7";
+                }
+
+                if (GUI.Button(new Rect(110, 245, 100, 100), "8"))
+                {
+                    input = input + "8";
+                }
+
+                if (GUI.Button(new Rect(215, 245, 100, 100), "9"))
+                {
+                    input = input + "9";
+                }
+
+                if (GUI.Button(new Rect(110, 350, 100, 100), "0"))
+                {
+                    input = input + "0";
+                }
+            }
+        }
+    }
+
+}
